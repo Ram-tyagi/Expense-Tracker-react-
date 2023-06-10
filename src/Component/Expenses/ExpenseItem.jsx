ede158d2a1582b2ae2be2ef7b7e7ab6ce067cc0d
Ram-tyagi <ramtyagi04@gmail.com>
2023-06-11 01:39:16 +0530

    Amount change using State

--- src/Component/Expenses/ExpenseItem.jsx
+++ src/Component/Expenses/ExpenseItem.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React,{useState} from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
+  const [amount, setAmount] = useState(props.amount);
+
+  const clickHandler = () => {
+    setAmount("$100");
+    console.log("Clicked");
+  }
   
   return (
     <Card className="expense-item">
@@ -13,8 +19,9 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
        
         <div className="expense-item__description"><h3>{props.location}</h3></div>
-        <div className="expense-item__price">{props.amount}</div>
+        <div className="expense-item__price">{amount}</div>
       </div>
+      <button onClick={clickHandler}  className="expense-item__price">Change Expense</button>
     </Card>
   );
 };
